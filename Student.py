cb5204fc344bdd2573dc215167191085b616a7a9
sam-2108 <okochibwire296@gmailcom>
2026-03-04 02:21:29 +0300

    Night practice

---
+++ Student.py
@@ -0,0 +1,4 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+course=input("What is your course? ")
+print(f"Hello {name}, you are {age} years old and you are taking {course}.")
