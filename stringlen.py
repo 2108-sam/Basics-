ad9270f9d30570e1bf4f1a8c0062ed7b226323c4
sam-2108 <okochibwire296@gmailcom>
2026-03-04 03:41:58 +0300

    Trial

--- stringlen.py
+++ stringlen.py
@@ -1,2 +1,4 @@
 word=input("Enter the word: ")
-print(f"You have entered {len(word)} word(s)")
+words=word.split()
+print(f"You have entered {len(word)} letter(s)")
+print(f"You have {len(words)} word(s)")
