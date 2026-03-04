80c1f8b0a13971b4850de2ed24cab804eaa5c3ce
sam-2108 <okochibwire296@gmailcom>
2026-03-04 04:07:38 +0300

    Trial

---
+++ state.py
@@ -0,0 +1,7 @@
+number=int(input("Enter a number: "))
+if number <0:
+    print(f"{number} is a negative number.")
+elif number>0:
+    print(f"{number} is positive.")
+else:
+    print(f"{number} is zero.")
